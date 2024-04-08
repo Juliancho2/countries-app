1a1ac4d4ff91b22a5b418dcf7b48def4018771bd
julian <julianmoreno242@gmail.com>
2024-04-07 21:41:27 -0500

    chore: configured layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,9 @@
 <script>
-    import '../app.css';
+	import Header from '../components/Header/Header.svelte';
+    import {mode} from '../lib/store.ts';
+    import '../styles/app.css';
 </script>
+<Header/>
+<main class="{ $mode === 'dark' ? 'dark' : ''}">
+    <slot/>
+</main>
