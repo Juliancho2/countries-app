9a791c404dd3069144bdcbb600e7f32655c9e064
julian <julianmoreno242@gmail.com>
2024-04-07 21:00:01 -0500

    feat: added darkmode

---
+++ src/components/ThemeMode/ThemeMode.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Svg from '../Svg/Svg.svelte';
+    import { mode } from '$lib/store.ts';
+	let darkMode = false;
+	const toggleDarkMode = () => {
+		darkMode = !darkMode;
+        mode.set(darkMode ? 'dark' : 'light');
+	};
+</script>
+<button class="hover:cursor-pointer box-content bg-gray-400 transition-all rounded-full p-2" on:click={toggleDarkMode}>
+    {#if darkMode}
+        <Svg
+            viewBox="0 0 256 256"
+            stroke="white"
+            width="1.2em"
+            height="1.2em"
+            fill="white"
+            icon="M120 40V16a8 8 0 0 1 16 0v24a8 8 0 0 1-16 0m72 88a64 64 0 1 1-64-64a64.07 64.07 0 0 1 64 64m-16 0a48 48 0 1 0-48 48a48.05 48.05 0 0 0 48-48M58.34 69.66a8 8 0 0 0 11.32-11.32l-16-16a8 8 0 0 0-11.32 11.32Zm0 116.68l-16 16a8 8 0 0 0 11.32 11.32l16-16a8 8 0 0 0-11.32-11.32M192 72a8 8 0 0 0 5.66-2.34l16-16a8 8 0 0 0-11.32-11.32l-16 16A8 8 0 0 0 192 72m5.66 114.34a8 8 0 0 0-11.32 11.32l16 16a8 8 0 0 0 11.32-11.32ZM48 128a8 8 0 0 0-8-8H16a8 8 0 0 0 0 16h24a8 8 0 0 0 8-8m80 80a8 8 0 0 0-8 8v24a8 8 0 0 0 16 0v-24a8 8 0 0 0-8-8m112-88h-24a8 8 0 0 0 0 16h24a8 8 0 0 0 0-16"
+        />
+    {:else}
+        <Svg width="1.2em" height="1.2em" stroke="white" viewBox="0 0 256 256" fill="white" icon="M233.54 142.23a8 8 0 0 0-8-2a88.08 88.08 0 0 1-109.8-109.8a8 8 0 0 0-10-10a104.84 104.84 0 0 0-52.91 37A104 104 0 0 0 136 224a103.09 103.09 0 0 0 62.52-20.88a104.84 104.84 0 0 0 37-52.91a8 8 0 0 0-1.98-7.98m-44.64 48.11A88 88 0 0 1 65.66 67.11a89 89 0 0 1 31.4-26A106 106 0 0 0 96 56a104.11 104.11 0 0 0 104 104a106 106 0 0 0 14.92-1.06a89 89 0 0 1-26.02 31.4"/>
+    {/if}
+</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content:  ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        'dark-primary': 'hsl(209, 23%, 22%)',
+        'light-primary': '#FAFAFA',
+        'dark-secondary': 'hsl(207, 26%, 17%)',
+        'light-secondary': '#ffff',
+      },
+      textColor:{
+        'dark-text': 'hsl(0, 0%, 100%)',
+        'light-text': 'hsl(200, 15%, 8%)',
+        'dark-text-light': 'hsl(200, 15%, 8%)',
+        'light-text-light': 'hsl(0, 0%, 52%)',
+      }
+    },
   },
   plugins: [],
 }
